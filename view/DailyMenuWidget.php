0a091ded8ba2202dfa51648a4f92521b570cfdac
dr41nU <dr41nU@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-16 22:05:41 +0000

    Stub for the widget
    git-svn-id: https://plugins.svn.wordpress.org/daily-menu/trunk@1046215 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view/DailyMenuWidget.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Adds Foo_Widget widget.
+ */
+class DailyMenuWidget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+				'dmWidget', // Base ID
+				__( 'Daily Menu', DM_DOMAIN_NAME), // Name
+				array( 'description' => __( 'Daily Menu Widget shows the menu of the day', DM_DOMAIN_NAME), ) // Args
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$menuArray = ListMenus::getWeekMenus();
+		echo __( 'Hello, World!', 'text_domain' );
+
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+	}
+
+}
